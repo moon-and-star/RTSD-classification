a0eb5da1f77edd1754130bc3c48e85a923effb0e
Ekaterina <tel.moon.and.star@gmail.com>
2017-07-16 16:29:12 +0300

    bugfix

--- util/config.py
+++ util/config.py
@@ -8,49 +8,53 @@ def getInitialConfig():
 	config['randseed'] = 42
 
 	im = {}
-	im["uncut_path"] = "../global_data/Traffic_signs/RTSD/imgs"
-	im["marking_path"] = "../global_data/Traffic_signs/RTSD"
-	im["classmark_prefix"] = "classmarking"
-	im["cropped_path"] = "../global_data/Traffic_signs/RTSD/classification"
-	im["processed_path"] = "./local_data/RTSD"
-	im["img_size"] = 32
-	im["padding"] = 4
-	im["border"] = 'replicate' # black, grey; replicate
-	im["min_class_size"] = 300
-	im["test_ratio"] = 0.2
-	im["val_ratio"] = 0.1
-	config["img"] = im
+	im['uncut_path'] = '../global_data/Traffic_signs/RTSD/imgs'
+	im['marking_path'] = '../global_data/Traffic_signs/RTSD'
+	im['classmark_prefix'] = 'classmarking'
+	im['cropped_path'] = '../global_data/Traffic_signs/RTSD/classification'
+	im['processed_path'] = './local_data/RTSD'
+	im['img_size'] = 32
+	im['padding'] = 4
+	im['border'] = 'replicate' # black, grey; replicate
+	im['min_class_size'] = 300
+	im['test_ratio'] = 0.2
+	im['val_ratio'] = 0.1
+	config['img'] = im
 
 	exp = {}
-	exp["exp_path"] = "./Experiments"
-	exp["group"] = 0
-	exp["group_description"] = ""
-	exp["exp_num"] = 0
-	exp["exp_description"] = ""
-	config["curr_exp"] = exp
-
+	exp['exp_path'] = './Experiments'
+	exp['group'] = 0
+	exp['group_description'] = ''
+	exp['exp_num'] = 0
+	exp['exp_description'] = ''
+	config['curr_exp'] = exp
 	config['exp_list'] = [[0]] #group 0 contains only 1 experiment - 0 
 
-	
+	wrn = {}
+	wrn['width'] = 1
+	wrn['metablock_depth'] = 1 #number of blocks in one block of blocks (watch article)
+	config['wide_resnet'] = wrn
+
 	tp = {}
-	tp["gpu_num"] = 0
-	tp["batch_size"] = 128
-	tp["epoch_num"] = 400
-	tp["test_frequency"] = 1 
-	tp["lr_step"] = 80
-	config["train_params"] = tp
+	tp['gpu_num'] = 0
+	tp['batch_size'] = 128
+	tp['epoch_num'] = 400
+	tp['test_frequency'] = 1 
+	tp['lr_step'] = 80
+	config['train_params'] = tp
 
 	sp = {}
-	sp["base_lr"] = 0.1
-	sp["type"] = "sgd"
-	sp["momentum"] = 0.9
-	sp["weight_decay"] = 0.0005
-	sp["lr_policy"] = "step"
-	sp["gamma"] = 0.2
-	sp["solver_mode"] = "GPU" 
-	config["solver_params"] = sp
+	sp['base_lr'] = 0.1
+	sp['type'] = 'sgd'
+	sp['momentum'] = 0.9
+	sp['weight_decay'] = 0.0005
+	sp['lr_policy'] = 'step'
+	sp['gamma'] = 0.2
+	sp['solver_mode'] = 'GPU' 
+	config['solver_params'] = sp
 
 	return config
+	
 
 
 
@@ -60,14 +64,14 @@ def getConfig(path):
 		return json.load(f)
 
 def setConfig(path, config):
-	with open(path, "w") as out:
+	with open(path, 'w') as out:
 		json.dump(config, out, sort_keys=True, indent=4) 
 	
 
 def initConfig(path):
 	config = getInitialConfig()
 	setConfig(path, config) 
-	print("\nConfiguration file path: {}\n".format(path) )
+	print('\nConfiguration file path: {}\n'.format(path) )
 	pprint(getConfig(path))
 
 	return config
--- util/util.py
+++ util/util.py
@@ -25,19 +25,6 @@ def safe_mkdir(directory_name):
 
 
 
-def to_arg_name(s):
-    if s == "BATCH_SZ":
-        return "batch_size"
-    elif s == "drop":
-        return "dropout"
-    elif s == "EPOCH":
-        return "epoch"
-    elif s == "CONV_GROUP":
-        return 'conv_group'
-    else: 
-        return s
-
-
 
 class Params:
     def __setattr__(self, name, value):
@@ -53,79 +40,4 @@ class Params:
         return self.__getattr__(item)
 
 
-def ParseParams(param_path):
-    args = Params()
-    with open(param_path) as f:
-        for line in f:
-            if line != "\n":
-                line = line.strip()
-                s = line.split("=")
-                s[0] = to_arg_name(s[0])
 
-                if s[0] in ['GAMMA','LR', 'drop_ratio'] :
-                    args[s[0]] = float(s[1])
-                elif s[0] in ['activation', 'dropout', 'note']:
-                    args[s[0]] = s[1]
-                else:
-                    args[s[0]] = int(s[1])
-
-    for i in args.__dict__:
-        print(i, args[i])
-    return args
-
-
-
-def gen_parser():
-    description = """
-    DESCRIPTION:
-    This program generates network architectures and solver
-    for particular experiment and stores them into prototxt
-    files in special folder (which is specified in current code)
-    """
-    parser = argparse.ArgumentParser(description=description)
-
-    parser.add_argument("EXPERIMENT_NUMBER",type=int, 
-                        help='the number of current experiment with nets ')
-
-    parser.add_argument('-b','--batch_size',default=512, type=int, 
-                        help='size of batch for training (default=512)')
-
-    parser.add_argument('-lr','--learning_rate',default=1e-3, type=float, 
-                        help='size of batch for training (default=1e-3)')
-
-    parser.add_argument('-e','--epoch',default=100, type=int, 
-                        help='number of training epoch (default=100)')
-
-    parser.add_argument('-tf','--test_frequency',default=1, type=int, 
-                        help='test frequensy in epochs (default=1)')
-
-    parser.add_argument('-sn','--snap_epoch',default=10, type=int, 
-                        help='snap frequensy in epochs (default=10)')
-
-    parser.add_argument('-st','--step_epoch',default=10, type=int, 
-                        help='learning rate decrease frequensy in epochs (default=10)')
-
-    parser.add_argument('-g','--gamma',default=0.5, type=float, 
-                        help='learning rate decrease factor (default=0.5)')
-
-    parser.add_argument('-a','--activation',default="scaled_tanh", type=str, 
-                        help='activation function type (default = \'scaled_tanh\' )')
-
-    parser.add_argument('-cg','--conv_group',default=1, type=int, 
-                        help='how many groups of filters within conv layers (default = 1 )')
-    parser.add_argument('-tn','--trial_number',default=1, type=int, 
-                        help='number of trial for 1 net (default = 5 )')
-
-    parser.add_argument('-dr','--drop_ratio',default=0.5, type=float, 
-                        help='set dropout parameter (default = 0.5 )')
-    parser.add_argument('-d','--dropout',action="store_true", 
-                        help='enable dropout')
-
-
-    parser.add_argument('-p','--proto_pref',default="./Prototxt", type=str, 
-                        help='Path for saving prototxt files (common prefix for all experiments)')
-    parser.add_argument('-s', '--snap_pref',default="./snapshots", type=str, 
-                        help='Path for saving snapshot files (common prefix for all experiments)')
-
-
-    return parser
