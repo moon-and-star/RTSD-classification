5e7221aeabdf748f372c3bf209a23d494d896c84
Ekaterina <tel.moon.and.star@gmail.com>
2017-07-18 01:19:21 +0300

    bugfix

--- netprocess/wresnet.py
+++ netprocess/wresnet.py
@@ -10,99 +10,165 @@ sys.path.append(str(absolute_path))
 
 
 from util.util import safe_mkdir, load_image_mean
-# from gen_solver import GenSingleNetSolver
+from solver import gen_solver, proto_path
 
 import caffe
 from caffe import layers as L
 from caffe import params as P
 from caffe.proto import caffe_pb2
 
-# def conv_relu(n, name, bottom, kernel_size, num_output, stride=1, pad=1, group=1):
-#     conv = L.Convolution(
-#         bottom,
-#         kernel_size=kernel_size,
-#         stride=stride,
-#         num_output=num_output,
-#         pad=pad,
-#         group=group,
-#         weight_filler = dict(type = 'xavier'),
-#         name = name,
-#     )
-
-#     setattr(n, name, conv)
-
-#     return conv, L.ReLU(conv, in_place = True, name = "{}_relu".format(name))
 
 
-# def conv_stanh(n, name, bottom, kernel_size, num_output, stride=1, pad=1, group=1):
-#     conv = L.Convolution(
-#         bottom,
-#         kernel_size=kernel_size,
-#         stride=stride,
-#         num_output=num_output,
-#         pad=pad,
-#         group=group,
-#         weight_filler = dict(type = 'xavier'),
-#         name = name,
-#     )
-#     setattr(n, name, conv)
-
-    
-#     scale1 = L.Scale(conv, in_place = True, name = "{}_prescale".format(name),
-#                      param=dict(lr_mult=0, decay_mult=0),
-#                      scale_param=dict(filler=dict(value=0.6666), bias_term=False)
-#                      )
-#     tanh =  L.TanH(scale1, in_place = True, name = "{}_sTanH".format(name))
-#     scale2 = L.Scale(tanh, in_place = True, name = "{}_postscale".format(name),
-#                      param=dict(lr_mult=0, decay_mult=0),
-#                      scale_param=dict(filler=dict(value=1.7159), bias_term=False)
-#                      )
-
-#     return conv, scale2
-
-
-# def conv1(n, name, bottom, num_output, kernel_size = 3, pad = None, activ = "relu", group=1):
-#     if pad is None: pad = kernel_size / 2
-#     if activ == "relu":
-#         conv1, relu1 = conv_relu(n, "{}".format(name), bottom, kernel_size, num_output, pad=pad, group=group)
-#         return relu1
-#     elif activ == "scaled_tanh":
-#         conv1, scale2 = conv_stanh(n, "{}".format(name), bottom, kernel_size, num_output, pad=pad, group=group)
-#         return scale2
+def get_caffe_phase(phase):
+    if phase == "train":
+        return "TRAIN"
+    elif phase == "val":
+        return "TEST"
+    elif phase == "test":
+        return "TEST"
     
 
-
-def avepool(name, bottom, kernel_size = 2, stride = 2):
-    return L.Pooling(bottom, kernel_size = kernel_size, stride = stride, pool = P.Pooling.AVE, name = name)
-
-
-def fc(name, bottom, num_output, activ="relu"):
-    fc = L.InnerProduct(
-        bottom,
-        num_output = num_output,
-        weight_filler = dict(type = 'xavier'),
-        name = "{}_{}".format(name, num_output)
-    )
-
-    if activ=="relu":
-        return fc, L.ReLU(fc, in_place = True, name = "{}_relu".format(name))
-
-    elif activ=="scaled_tanh":
-        scale1 = L.Scale(fc, in_place = True, name = "{}_prescale".format(name),
-                         param=dict(lr_mult=0, decay_mult=0),
-                         scale_param=dict(filler=dict(value=0.6666), bias_term=False))
-        tanh =  L.TanH(scale1, in_place = True, name = "{}_sTanH".format(name))
-        scale2 = L.Scale(tanh, in_place = True, name = "{}_postscale".format(name),
-                         param=dict(lr_mult=0, decay_mult=0),
-                         scale_param=dict(filler=dict(value=1.7159), bias_term=False))
-        # scale2 =  L.TanH(fc, in_place = True, name = "{}_sTanH".format(name))
-        return fc, scale2
-
-    elif activ=="softmax":
-        # return fc, L.Softmax(fc, in_place=True)
-        return fc, L.Softmax(fc, in_place=False)
+def get_transform_param(mean, img_size):
+    if mean is not None:
+        return dict(mirror=False, crop_size=img_size , mean_value = map(int, mean), scale=1.0/255)
     else:
-        return fc
+        return dict(mirror=False, crop_size=img_size , scale=1.0/255)
+        
+
+def append_data(net, phase, **kwargs):
+    img_size, img_path = kwargs['img_size'], kwargs['img_path']
+    batch_size, pad = kwargs['batch_size'], kwargs['pad']
+
+    mean_path = '{img_path}/{phase}/mean.txt'.format(**locals())
+    mean = load_image_mean(mean_path)
+
+    image_data_param = dict(
+        source = '{img_path}/gt_{phase}.txt'.format(**locals()), 
+        batch_size = batch_size,
+        new_height = img_size + 2 * pad,
+        new_width = img_size + 2 * pad)
+
+    PHASE = get_caffe_phase(phase)
+    net['data'], net['label'] = L.ImageData(
+        image_data_param = image_data_param,
+        transform_param = get_transform_param(mean, img_size),
+        ntop = 2,
+        include = dict(phase = caffe_pb2.Phase.Value(PHASE)),
+        name = "data")
+
+    return net.data, net.label
+
+
+
+def convolution(bottom, **kwargs):
+    return L.Convolution(bottom,
+        kernel_size=kwargs['kernel_size'],
+        stride=kwargs['stride'],
+        num_output=kwargs['num_output'],
+        pad=kwargs['pad'],
+        group=kwargs['group'],
+        weight_filler = dict(type = 'xavier'),
+        name = kwargs['name'])    
+
+
+
+def bn_relu_conv(bottom, **kwargs):
+    bn = L.BatchNorm(bottom, use_global_stats=True, in_place=True)
+    relu = L.ReLU(bn, in_place = True)
+
+    # bn = L.BatchNorm(bottom, use_global_stats=True)
+    # relu = L.ReLU(bn)
+    conv = convolution(relu, **kwargs)
+
+    return conv
+
+
+def append_conv(net, bottom, **kwargs):
+    # conv = convolution(bottom, **kwargs)
+    conv = bn_relu_conv(bottom, **kwargs)
+    setattr(net, kwargs['name'], conv)
+    return conv
+
+
+
+
+def c1_layer(bottom, **kwargs):
+    kwargs["name"] = kwargs['block_name'] + '_c1'
+
+    if kwargs['downsample']:
+        kwargs['stride'] = kwargs['downsampling_stride']
+        conv = bn_relu_conv(bottom, **kwargs)
+    else:
+        conv = bn_relu_conv(bottom, **kwargs)
+
+    return conv
+
+
+def c2_layer(bottom, **kwargs):
+    kwargs["name"] = kwargs['block_name'] + '_c2'
+    return bn_relu_conv(bottom, **kwargs)
+
+
+def shortcut_layer(bottom, **kwargs):
+    if kwargs['downsample']:
+        kwargs['kernel_size'] = 1
+        kwargs['stride'] = kwargs['downsampling_stride']
+        kwargs['pad'] = 0
+        kwargs['name'] = kwargs['name'] + '_shortcut'
+        return convolution(bottom, **kwargs)
+    else:
+        return bottom
+
+
+def sum_layer(layer1, layer2, name):
+    return L.Eltwise(layer1, layer2, 
+        name = name,
+        eltwise_param = dict(operation=1)) # 1 means sum
+
+
+
+def basic_block(bottom, **kwargs):
+    conv1 = c1_layer(bottom, **kwargs)
+    conv2 = c2_layer(conv1, **kwargs)
+    shortcut = shortcut_layer(bottom, **kwargs)
+
+    out = sum_layer(shortcut, conv2, name=kwargs['block_name'] + '_sum')
+    return out
+
+
+
+def append_conv_block(net, bottom, **kwargs):
+    for i in range(kwargs['depth']):
+        downsample = (i == 0) and (kwargs['downsample'] == True)
+        kwargs['downsample'] = downsample
+        kwargs['block_name'] = '{}_b{}'.format(kwargs['name'], i+1)
+        bottom = basic_block(bottom, **kwargs)
+
+    setattr(net, kwargs['name'], bottom)
+    return bottom
+
+
+
+def bn_relu_avepool(bottom, **kwargs ):
+    bn = L.BatchNorm(bottom, use_global_stats=True, in_place=True)
+    relu = L.ReLU(bn, in_place = True)
+    # bn = L.BatchNorm(bottom, use_global_stats=True)
+    # relu = L.ReLU(bn)
+    return L.Pooling(relu, **kwargs)
+
+
+def append_pool(net, bottom, **kwargs):
+    pool = bn_relu_avepool(bottom, **kwargs)
+    setattr(net, kwargs['name'], pool)    
+
+    return pool
+
+
+def append_fc(net, bottom, **kwargs):
+    fc = L.InnerProduct(bottom, **kwargs)
+    setattr(net, kwargs['name'], fc) 
+
+    return fc
 
 
 def dropout(name, bottom, dropout_ratio):
@@ -114,84 +180,165 @@ def dropout(name, bottom, dropout_ratio):
     )
 
 
+
+def append_softmax(net, bottom):
+    activ = L.Softmax(bottom, in_place=False)
+    setattr(net, 'softmax', activ)
+    return activ    
+
+
 def accuracy(name, bottom, labels, top_k):
     return L.Accuracy(
         bottom,
         labels,
         accuracy_param = dict(top_k = top_k),
-        # include = dict(phase = caffe_pb2.Phase.Value("TEST")),
         name = name
     )
 
-
-
-
-
-
-
-def NumOfClasses(dataset):
-    # set_size = {"rtsd-r1": 67, "rtsd-r3": 106, 'RTSD': 116}
-    # return set_size[dataset]
-    pass
-
-
-def ConvPoolAct(n, args):
-    n.pool1 = maxpool("pool1", conv1(n, "conv1", n.data, 100, kernel_size = 7, pad = 0, 
-                        activ=args.activation, group=1))
-    n.pool2 = maxpool("pool2", conv1(n, "conv2", n.pool1, 150, kernel_size = 4, pad = 0, 
-                        activ=args.activation, group=args.conv_group))
-    n.pool3 = maxpool("pool3", conv1(n, "conv3", n.pool2, 250, kernel_size = 4, pad = 0, 
-                        activ=args.activation, group=args.conv_group))
-
-
-
-def FcDropAct(n, args, dataset):
-    num_of_classes = NumOfClasses(dataset)
-
-    n.fc4_300, n.relu4 = fc("fc4", n.pool3, num_output = 300, activ=args.activation)
-    if args.dropout == True:
-        n.drop4 = dropout("drop4", n.relu4, dropout_ratio = args.drop_ratio)
-
-    n.fc5_classes, n.softmax = fc("fc5", n.relu4, num_output = num_of_classes, activ="softmax")
-
-
-
-#needed for commitee testing
-def NoLMDB_Net(args, dataset, mode, phase):
-    data_prefix = "../local_data"
-    mean_path = '{}/lmdb/{}/{}/{}/mean.txt'.format(data_prefix,dataset, mode, phase)
-    # mean_path = '{}/{}/{}/{}/mean.txt'.format(data_prefix,dataset, mode, phase)
-    src_path = PrepareSrcFromGT(data_prefix, dataset, mode, phase) 
-
-    n = caffe.NetSpec()
-    # DataOnly(n)
-    DataOnly(n, 'test', src_path, mean_path)
-    ConvPoolAct(n, args)
-    FcDropAct(n, args, dataset)
-    
-    return n.to_proto()
-
-
-
-# def make_net(args, dataset, mode, phase):
-#     data_prefix = "../local_data"
-#     mean_path = '{}/lmdb/{}/{}/{}/mean.txt'.format(data_prefix,dataset, mode, phase)
-#     mean_path = '{}/{}/{}/{}/mean.txt'.format(data_prefix,dataset, mode, phase)
-#     lmdb = '{}/lmdb/{}/{}/{}/lmdb'.format(data_prefix, dataset, mode, phase)  
-
-#     n = caffe.NetSpec()
-#     Data(n, lmdb, phase, args.batch_size, mean_path)
-#     ConvPoolAct(n, args)
-#     FcDropAct(n, args, dataset)
-    
-#     n.loss = L.MultinomialLogisticLoss(n.softmax, n.label)
-#     n.accuracy_1 = accuracy("accuracy_1", n.fc5_classes, n.label, 1)
-#     n.accuracy_5 = accuracy("accuracy_5", n.fc5_classes, n.label, 5)
+# def append_loss(net, bottom, label, phase):
+#     n.loss = L.MultinomialLogisticLoss(bottom, label)
+#     n.accuracy_1 = accuracy("accuracy_1", bottom, label, 1)
+#     n.accuracy_5 = accuracy("accuracy_5", bottom, label, 5)
 
 #     if phase == "train":
 #         n.silence = L.Silence(n.accuracy_1, n.accuracy_5, ntop=0)
 
-#     return n.to_proto()
+
+def append_loss(net, bottom, label, phase):
+    net.loss = L.MultinomialLogisticLoss(bottom, label, ntop=1)
+    
+    if phase != "train":
+        net.accuracy_1 = accuracy("accuracy_1", bottom, label, 1)
+        net.accuracy_5 = accuracy("accuracy_5", bottom, label, 5)
+
+    return net.loss
+
+
+
+
+def data_args(config):
+    args = {}
+    args['img_path'] = config['img']['processed_path']
+    args['img_size'] = config['img']['img_size']
+    args['pad'] = config['img']['padding']
+    args['batch_size'] = config['train_params']['batch_size']
+
+    return args
+
+
+def conv1_args(config=None):
+    args = {}
+    args['num_output'] = 16
+    args['name'] = 'conv1'
+    args['kernel_size'] = 3
+    args['stride'] = 1
+    args['pad'] = 1
+    args['group'] = 1
+    return args
+
+
+def conv2_args(config):
+    args = {}
+   
+    args['name'] = 'conv2'
+    args['kernel_size'] = 3
+    args['stride'] = 1
+    args['pad'] = int(args['kernel_size'] / 2)
+    args['group'] = 1
+
+    args['depth'] = config['wide_resnet']['metablock_depth']
+    args['num_output'] = config['wide_resnet']['width'] * 16
+    args['downsample'] = False 
+
+    return args
+
+
+def conv3_args(config):
+    args = {}
+    
+    args['name'] = 'conv3'
+    args['kernel_size'] = 3
+    args['stride'] = 1
+    args['pad'] = int(args['kernel_size'] / 2)
+    args['group'] = 1
+
+    args['depth'] = config['wide_resnet']['metablock_depth']
+    args['num_output'] = config['wide_resnet']['width'] * 32
+    args['downsample'] = True
+    args['downsampling_stride'] = 2
+
+    return args
+
+
+def conv4_args(config):
+    args = {}
+    
+    args['name'] = 'conv4'
+    args['kernel_size'] = 3
+    args['stride'] = 1
+    args['pad'] = int(args['kernel_size'] / 2)
+    args['group'] = 1
+
+    args['depth'] = config['wide_resnet']['metablock_depth']
+    args['num_output'] = config['wide_resnet']['width'] * 64
+    args['downsample'] = True
+    args['downsampling_stride'] = 2
+
+    return args
+
+
+def avgpool_args():
+    args = {}
+    args['name'] = 'avg_pool'
+    args['kernel_size'] = 8
+    args['stride'] = 1
+
+    return args
+
+
+def NumOfClasses(config):
+    gt_path = '{}/gt_train.txt'.format(config['img']['processed_path'])
+
+    with open(gt_path) as f:
+        classes = set()
+        for line in f.readlines():
+            s = line.split(' ')
+            classes.add(s[1].replace('\n', ''))
+
+        return len(classes)
+
+
+def fc_args(config):
+    args = {}
+    args['name'] = 'fc'
+    args['num_output'] = NumOfClasses(config)
+    args['weight_filler'] = dict(type = 'xavier')
+
+    return args
+
+
+
+def wresnet(config, phase):
+    net = caffe.NetSpec()
+
+    data, label = append_data(net, phase, **data_args(config))
+    conv1 = append_conv(net, data, **conv1_args(config))
+    conv2 = append_conv_block(net, conv1, **conv2_args(config))
+    conv3 = append_conv_block(net, conv2, **conv3_args(config))
+    conv4 = append_conv_block(net, conv3, **conv4_args(config))
+    pool = append_pool(net, conv4, **avgpool_args())
+
+    
+    fc = append_fc(net, pool, **fc_args(config))
+    softmax = append_softmax(net, fc)
+    loss = append_loss(net, softmax, label, phase)
+
+    return net.to_proto()
+
+
+
+
+
 
 
 
@@ -219,99 +366,15 @@ def launch():
 
 
 
-# def Data(net, img_path=None, img_size=32, batch_size=512, pad=4):
-def Data(net, **kwargs):
-    img_size, img_path = kwargs['img_size'], kwargs['img_path']
-    batch_size, pad = kwargs['batch_size'], kwargs['pad']
-
-    for phase in ['train', 'val']:
-        mean_path = '{img_path}/{phase}/mean.txt'.format(**locals())
-        mean = load_image_mean(mean_path)
-
-        if mean is not None:
-            transform_param = dict(mirror=False, crop_size=img_size , mean_value = map(int, mean), scale=1.0/255)
-        else:
-            transform_param = dict(mirror=False, crop_size=img_size , scale=1.0/255)
-
-        if phase == "train":
-            PHASE = "TRAIN"
-        elif phase == "val":
-            PHASE = "TEST"
-
-        image_data_param = dict(
-            source = '{img_path}/gt_{phase}.txt'.format(**locals()), 
-            batch_size = batch_size,
-            new_height = img_size + 2 * pad,
-            new_width = img_size + 2 * pad)
-
-        net.data, net.label = L.ImageData(
-            image_data_param = image_data_param,
-            transform_param = transform_param,
-            ntop = 2,
-            include = dict(phase = caffe_pb2.Phase.Value(PHASE)),
-            name = "data")
-     
-# def conv_relu(n, name, bottom, kernel_size, num_output, stride=1, pad=1, group=1):
-#     conv = L.Convolution(
-#         bottom,
-#         kernel_size=kernel_size,
-#         stride=stride,
-#         num_output=num_output,
-#         pad=pad,
-#         group=group,
-#         weight_filler = dict(type = 'xavier'),
-#         name = name,
-#     )
-
-#     setattr(n, name, conv)
-
-#     return conv, L.ReLU(conv, in_place = True, name = "{}_relu".format(name))
-
-
-def conv1(net, name, bottom, num_output, kernel_size=3, stride=1, pad=1, group=1):
-    conv = L.Convolution(bottom,
-        kernel_size=kernel_size,
-        stride=stride,
-        num_output=num_output,
-        pad=pad,
-        group=group,
-        weight_filler = dict(type = 'xavier'),
-        name = name)
-
-    setattr(n, name, conv)
-
-    return conv, L.ReLU(conv, in_place = True, name = "{}_relu".format(name))
-    
-
-
-def wresnet(data_args):
-    net = caffe.NetSpec()
-
-    data_args = get_data_args(config)
-    Data(net, **data_args)
-
-    conv1(net)
-    # conv2(net)
-    # conv3(net)
-    # conv4(net)
-    # avg_pool
-
-
-
-def get_data_args(config):
-    args = {}
-    args['img_path'] = config['img']['processed_path']
-    args['img_size'] = config['img']['img_size']
-    args['pad'] = config['img']['padding']
-    args['batch_size'] = config['train_params']['batch_size']
-
-    return args
 
 
 
 def construct(config):
-    wrn = wresnet(config)
-    pass
+    for phase in ['train', 'val', 'test']:
+        wrn = wresnet(config, phase)
+        with open("./{}.prototxt".format(phase), 'w') as out:
+            out.write(str(wrn))
+
 
 
 
@@ -320,5 +383,4 @@ from pprint import pprint
 
 if __name__ == '__main__':
     config = getConfig('./config.json')
-    pprint(config)
     construct(config)
