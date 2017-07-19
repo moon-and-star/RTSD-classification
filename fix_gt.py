16f700a984de0f8233c95f6343e8ea0e28aec9e2
Ekaterina <tel.moon.and.star@gmail.com>
2017-07-19 16:25:26 +0300

    for 1 image in set

--- fix_gt.py
+++ fix_gt.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 for phase in ['train', 'val', 'test']:
-	with open('./local_data/RTSD/gt_{}.txt'.format(phase)) as f:
+	with open('./local_data/RTSD/gt_{}_full.txt'.format(phase)) as f:
 		lines = f.readlines()
 
 	with open('./local_data/RTSD/gt_{}.txt'.format(phase), 'w') as f:
-		for line in lines:
+		for line in lines[:1]:
 			f.write('./local_data/RTSD/{phase}/{line}'.format(**locals()))
