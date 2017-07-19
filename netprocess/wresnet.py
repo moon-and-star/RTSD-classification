646d957c5bf5d28ed234bed407e333b0149c5c90
Ekaterina <tel.moon.and.star@gmail.com>
2017-07-19 17:44:05 +0300

    bugfix

--- netprocess/wresnet.py
+++ netprocess/wresnet.py
@@ -335,19 +335,16 @@ def wresnet(config, phase):
     net = caffe.NetSpec()
 
     data, label = append_data(net, phase, **data_args(config))
-    # net.silence = L.Silence(data, ntop=0)
-    conv1 = append_conv(net, data, **conv1_args(config))
-    conv2 = append_conv_block(net, conv1, **conv2_args(config))
-    conv3 = append_conv_block(net, conv2, **conv3_args(config))
-    conv4 = append_conv_block(net, conv3, **conv4_args(config))
-    pool = append_pool(net, conv4, **avgpool_args())
+    # conv1 = append_conv(net, data, **conv1_args(config))
+    # conv2 = append_conv_block(net, conv1, **conv2_args(config))
+    # conv3 = append_conv_block(net, conv2, **conv3_args(config))
+    # conv4 = append_conv_block(net, conv3, **conv4_args(config))
+    # pool = append_pool(net, conv4, **avgpool_args())
 
     
-    fc = append_fc(net, pool, **fc_args(config))
+    fc = append_fc(net, data, **fc_args(config))
     append_tail(net, fc, label, phase)
     
-    # fc = append_fc(net, pool, **fc_args(config))
-    # append_tail(net, fc, label, phase)
     return net.to_proto()
 
 
