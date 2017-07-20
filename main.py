b72d475b63f5f7b56185392e7a209111ec4ea4d5
Ekaterina <tel.moon.and.star@gmail.com>
2017-07-20 18:55:43 +0300

    bugfix

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+
+import sys
+
+import pathlib2 as pathlib
+local_path = pathlib.Path('./')
+absolute_path = local_path.resolve()
+sys.path.append(str(absolute_path))
+
+
+
 from util.cli import setupCLI
 
 if __name__ == '__main__':
