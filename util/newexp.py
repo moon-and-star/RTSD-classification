0c394afa0e9fe87ee00f29077d3c44d69d7ce8d2
Ekaterina <tel.moon.and.star@gmail.com>
2017-07-21 00:56:24 +0300

    complete version

--- util/newexp.py
+++ util/newexp.py
@@ -1,9 +1,73 @@
 #!/usr/bin/env python
+import sys
+import pathlib2 as pathlib
+
+local_path = pathlib.Path('./')
+absolute_path = local_path.resolve()
+sys.path.append(str(absolute_path))
+
+
+from config import get_config, set_config
+from util import safe_mkdir
+# from pprint import pprint
+# import json
+
+
+def update_group_list(config):
+    group = config['exp']['group']
+    experiments = config['group_list'][group]['experiments']
+
+    description = config['exp']['exp_description']
+    experiments.append({
+        'exp_description': description,
+        'exp_num': config['exp']['exp_num']})
+   
+
+def update_exp_num(config):
+    group = config['exp']['group']
+    experiments = config['group_list'][group]['experiments']
+    config['exp']['exp_num'] = len(experiments)
+    
+
+
+
+def update_config(args):
+    config = get_config(args.confpath)
+
+    config['exp']['exp_description'] = args.description
+    update_exp_num(config)
+    update_group_list(config)
+
+    set_config(args.confpath, config)
+   
+    
+def exp_dir(config):
+    root = config['exp']['exp_path']
+    group = config['exp']['group']
+    exp_num = config['exp']['exp_num']
+    return '{root}/group_{group}/exp_{exp_num}'.format(**locals())
+
+
+def update_dir(config):
+    path = exp_dir(config)
+    safe_mkdir(path)
+
+    with open('{path}/exp_description.txt'.format(**locals()), 'w') as f:
+        f.write(config['exp']['exp_description'])
+
+
 
 def newexp(args):
-    pass
+    update_config(args)
+    config = get_config(args.confpath)
+    update_dir(config)
+   
 
 def setupNewExpParser(subparsers):
     newexp_parser = subparsers.add_parser("newexp", help='- Sets experiment number to max_experiment_number + 1 (within current group)')
     newexp_parser.set_defaults(func=newexp)
+
+    newexp_parser.add_argument('-d','--description',action='store', default="NO_DESCRIPTION", type=str,
+                        help='add description for experiment')
+
 
