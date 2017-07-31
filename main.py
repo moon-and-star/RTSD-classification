ca2ac77233f671c5f4316de76bda9830aaaa18c1
Ekaterina <tel.moon.and.star@gmail.com>
2017-08-01 02:24:52 +0300

    adde python paths

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 
 
 import sys
+home = '/home/GRAPHICS2/20e_ame'
+sys.path.append("{home}/anaconda2/pkgs/pathlib2-2.2.1-py27_0/lib/python2.7/site-packages/".format(**locals()))
+sys.path.append("{home}/anaconda2/pkgs/scandir-1.4-py27_0/lib/python2.7/site-packages/".format(**locals()))
+
 import pathlib2 as pathlib
-local_path = pathlib.Path('./')
-absolute_path = local_path.resolve()
+absolute_path = pathlib.Path('./').resolve()
 sys.path.append(str(absolute_path))
 
 
