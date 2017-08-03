7fceb81a3ae2e276f3bc7a64a3aa8b490304f9af
Ekaterina <tel.moon.and.star@gmail.com>
2017-08-04 02:56:15 +0300

    new file

--- imgprocess/create_lmdb.py
+++ imgprocess/create_lmdb.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import os
+from util.util import safe_mkdir
 
 for phase in ['val', 'train', 'test']:
 	tools = '/opt/caffe/build/tools'
 	prefix = "./local_data/RTSD"
+	safe_mkdir('{prefix}/lmdb/{phase}'.format(**locals()))
 	os.system("GLOG_logtostderr=1 {tools}/convert_imageset -shuffle -backend lmdb\
 	 {prefix}/{phase} {prefix}/gt_{phase}.txt  {prefix}/lmdb/{phase}/lmdb".format(**locals()))
 
