2ed7b813b1a700cf2fbc4a6ba26873dfdfc43aef
Ekaterina <tel.moon.and.star@gmail.com>
2017-08-04 02:59:22 +0300

    bugfix

--- imgprocess/create_lmdb.py
+++ imgprocess/create_lmdb.py
@@ -22,7 +22,7 @@ for phase in ['val', 'train', 'test']:
 	prefix = "./local_data/RTSD"
 	safe_mkdir('{prefix}/lmdb/{phase}'.format(**locals()))
 	os.system("GLOG_logtostderr=1 {tools}/convert_imageset -shuffle -backend lmdb\
-	 {prefix}/{phase} {prefix}/gt_{phase}.txt  {prefix}/lmdb/{phase}/lmdb".format(**locals()))
+	 {prefix}/{phase}/ {prefix}/gt_{phase}.txt  {prefix}/lmdb/{phase}/lmdb".format(**locals()))
 
 # GLOG_logtostderr=1 $TOOLS/convert_imageset -shuffle -backend lmdb  \
 # 			     ${prefix}/${i}/${j}/${k}/          \
